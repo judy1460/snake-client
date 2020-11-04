9bc2c0f46787e64fab0a30069d5a6022c9e411c2
Mina <minatoosa@yahoo.com>
2020-11-04 05:58:08 +0000

    Add play snake

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
