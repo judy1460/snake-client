94c138eb8502462be13ac25f741c5066baa04c81
Mina <minatoosa@yahoo.com>
2020-11-07 19:05:48 +0000

    Add snake 2

--- play.js
+++ play.js
@@ -1,17 +1,3 @@
-
-const net = require('net');
-
-
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.2.21',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
-console.log('you ded cuz you idled');
+const { connect } = require('./client');
+console.log('Connecting ...');
 connect();
