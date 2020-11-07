d242f9d89d5682c21d603617173d16571371c726
Mina <minatoosa@yahoo.com>
2020-11-07 21:23:02 +0000

    add snake

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.2.21',
@@ -14,7 +15,7 @@ const connect = function() {
   });
 
   conn.on('connect', () => {
-    conn.write("Name: MYNA");
+    conn.write("Name: MYN");
   return conn;
 });
 }
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = '192.168.2.21' ;
+const PORT = 50541 ;
+
+module.exports = {
+  IP,
+  PORT
+};
