00bc07491565a629a0e20e0027b37847016b8bfb
Mina <minatoosa@yahoo.com>
2020-11-07 20:43:38 +0000

    ADD snake

--- play.js
+++ play.js
@@ -1,3 +1,10 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
